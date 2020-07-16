5326eb3f3680b943f2940e9eb87679a14ee94fe1
Angela Maugey <a.maugey@gmail.com>
2020-07-16 22:35:12 +0100

    basics of NavBar WIP

---
+++ frontend/src/components/navigation/NavBar.js
@@ -0,0 +1,45 @@
+import React from "react"
+import styles from "./NavBar.module.css"
+
+export default function NavBar() {
+  return (
+    <nav className={styles.container}>
+      <div className={styles.logoContainer}>
+        <a href="https://google.com">
+          <img
+            className={styles.logo}
+            src={require("../../assets/annie-logo.png")}
+          />
+        </a>
+      </div>
+      <ul className={styles.navLinkList}>
+        <li>
+          <a href="https://google.com">About</a>
+        </li>
+        <li>
+          <a href="https://google.com">Classes</a>
+        </li>
+        <li>
+          <a href="https://google.com">Retreats</a>
+          <ul className={styles.retreatMenu}>
+            <li>
+              <a href="https://google.com">Tuscan hills</a>
+            </li>
+            <li>
+              <a href="https://google.com">Fire and Water</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="https://google.com">Corporate</a>
+        </li>
+        <li>
+          <a href="https://google.com">Food</a>
+        </li>
+        <li>
+          <a href="https://google.com">Blog</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
