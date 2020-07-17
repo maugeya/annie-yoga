676b74c2e1c844df1cc68784a55345bf3cc03f30
Angela Maugey <a.maugey@gmail.com>
2020-07-17 09:50:24 +0100

    Moving settings

--- backend/annie_may_rice/settings.py
+++ backend/annie_may_rice/settings.py
@@ -15,12 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a!&ohi@+vn0_ce-94!-sh(*m0ykb-ott$ifn&!!derz*dydl^o'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
