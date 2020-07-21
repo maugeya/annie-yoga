bcea89f4a0a1da247706a19df45b2a4b094e25f1
Angela Maugey <a.maugey@gmail.com>
2020-07-21 22:46:11 +0100

    CorporateRoute and styles WIP

---
+++ frontend/src/components/corporate/CorporateRoute.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import Layout from "../layout/Layout"
+
+export default function CorporateRoute() {
+  return (
+    <Layout>
+      <div>CORPORATEROUTE</div>
+    </Layout>
+  )
+}
