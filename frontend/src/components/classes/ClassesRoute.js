607ce21bdaf27dfb67b9eb3f5c79baa4e8d3bbb6
Angela Maugey <a.maugey@gmail.com>
2020-07-21 22:45:56 +0100

    ClassesRoute and styles WIP

---
+++ frontend/src/components/classes/ClassesRoute.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import Layout from "../layout/Layout"
+
+export default function ClassesRoute() {
+  return (
+    <Layout>
+      <div>CLASSESROUTE</div>
+    </Layout>
+  )
+}
