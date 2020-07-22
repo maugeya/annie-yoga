70bc31652c4404d0a4c9fb938879fabdcc77a6e8
Angela Maugey <a.maugey@gmail.com>
2020-07-22 14:28:38 +0100

    creating custom hook for detecting scrollY in relation to vh

---
+++ frontend/src/hooks/useScroll.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react"
+
+export default function useScroll() {
+  const [scrollYPercentage, setScrollYPercentage] = useState(0)
+
+  function logScroll() {
+    setScrollYPercentage((window.pageYOffset / window.innerHeight) * 100)
+  }
+
+  useEffect(() => {
+    function watchScroll() {
+      window.addEventListener("scroll", logScroll)
+    }
+    watchScroll()
+    return () => {
+      window.removeEventListener("scroll", logScroll)
+    }
+  })
+  return scrollYPercentage
+}
