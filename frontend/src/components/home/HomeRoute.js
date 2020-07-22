4eb5185be2ee5801ba1c90bfc192f714c9c51fa6
Angela Maugey <a.maugey@gmail.com>
2020-07-22 16:26:54 +0100

    Adding text overlay on top of photo in HomeRoute component

--- frontend/src/components/home/HomeRoute.js
+++ frontend/src/components/home/HomeRoute.js
@@ -1,15 +1,19 @@
 import React from "react"
 
 import styles from "./HomeRoute.module.css"
+import About from "./About"
 
 export default function HomeRoute() {
   return (
     <div className={styles.container}>
-      <div className={styles.mainImage} />
-      <div className={styles.textOverlay}>
-        <h1 className={styles.annieTitle}>Annie May Rice</h1>
-        <h2>Yoga</h2>
+      <div className={styles.jumboContainer}>
+        <div className={styles.mainImage} />
+        <div className={styles.textOverlay}>
+          <h1 className={styles.annieTitle}>Annie May Rice</h1>
+          <h2>Yoga</h2>
+        </div>
       </div>
+      <About />
     </div>
   )
 }
