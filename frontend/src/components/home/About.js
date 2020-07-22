15106664f30efef60244ff538944c8313c330fcf
Angela Maugey <a.maugey@gmail.com>
2020-07-22 19:55:23 +0100

    Updating content of About component

--- frontend/src/components/home/About.js
+++ frontend/src/components/home/About.js
@@ -1,25 +1,36 @@
 import React from "react"
 
 import styles from "./About.module.css"
+import Layout from "../layout/Layout"
 
 export default function About() {
   return (
-    <div className={styles.container}>
-      <div className={styles.sectionTitle}>ABOUT ME</div>
-      <div>
-        The Yoga Practice in association with Little Greene Yoga has been
-        running adult yoga classes in the South Side of Glasgow, Scotland for
-        the past 15 years. Classes are popular and are booked on a 6 week block
-        basis, though there is the option to book a drop-in place if one is
-        available. The Yoga Practice operates a booking system to control the
-        class numbers therefore ensuring the comfort and safety of all students.
-        Veronica Greene, Founder of Little Greene Yoga and the Yoga Practice,
-        qualified as an Iyengar yoga instructor (Junior Intermediate Level 1).
-        She is a fully qualified Children’s yoga instructor and a Senior Yoga
-        Instructor under Yoga Alliance. Little Greene Yoga in association with
-        The Yoga Practice is the only Registered Children’s Yoga School in
-        Scotland.{" "}
-      </div>
-    </div>
+    <Layout>
+      <section id="#about" className={styles.container}>
+        <div className={styles.sectionTitle}>
+          <h1>ABOUT ME</h1>
+        </div>
+        <div className={styles.sectionContent}>
+          <p>
+            I believe that nearly everyone would be a bit better off with some
+            yoga in their lives in whatever form works for them. It doesn’t
+            matter what it looks like and it doesn’t matter where you do it, how
+            you do it or what brand of yoga mat you have, I think it’s just
+            about giving yourself some precious time to practice some yoga. I
+            believe everyone should have some access to yoga and hope that I can
+            help spread it to more people in more ways.
+          </p>
+          <p>
+            I teach all around London in public group classes as well as in
+            people’s work spaces and homes.
+          </p>
+          <p>
+            If you’d like to get in touch about anything, please do. You can
+            email me at <span>anniemayrice@gmail.com</span>
+          </p>
+          <p>PEACE.</p>
+        </div>
+      </section>
+    </Layout>
   )
 }
