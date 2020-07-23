1a533ef01ed0d7b5c2b70042389388f54aa77ec2
Angela Maugey <a.maugey@gmail.com>
2020-07-23 23:04:55 +0100

    Adding background image corporate route

--- frontend/src/components/corporate/CorporateRoute.js
+++ frontend/src/components/corporate/CorporateRoute.js
@@ -1,11 +1,18 @@
 import React from "react"
 
 import Layout from "../layout/Layout"
+import styles from "./CorporateRoute.module.css"
 
 export default function CorporateRoute() {
   return (
-    <Layout>
-      <div>CORPORATEROUTE</div>
-    </Layout>
+    <div className={styles.container}>
+      <div className="jumboContainer">
+        <div className={styles.mainImage} />
+        <div className="centerTextOverlay">
+          <h1 className="animatedUnderline">Corporate Yoga</h1>
+          <h2>Get Involved</h2>
+        </div>
+      </div>
+    </div>
   )
 }
