a1f20de5b466b337978bde6f93c189f7df5bc659
Angela Maugey <a.maugey@gmail.com>
2020-07-25 22:20:48 +0100

    Adding new app to installed apps

--- backend/annie_may_rice/settings.py
+++ backend/annie_may_rice/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'yoga_classes',
 ]
 
 MIDDLEWARE = [
@@ -94,9 +95,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-GB'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/London'
 
 USE_I18N = True
 
