600f33ff7a4718e12c0a093a7b3e46ab5af02b4f
Angela Maugey <a.maugey@gmail.com>
2020-07-26 10:33:08 +0100

    useEffect created for API call

--- frontend/src/components/classes/ClassesRoute.js
+++ frontend/src/components/classes/ClassesRoute.js
@@ -1,8 +1,26 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 
 import Layout from "../layout/Layout"
 
 export default function ClassesRoute() {
+  const [yogaClasses, setYogaClasses] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch("http://localhost:8000/classes/")
+      res
+        .json()
+        .then((res) => {
+          setYogaClasses(res)
+          setIsLoading(false)
+        })
+        .catch((err) => setError(err))
+    }
+
+    fetchData()
+  }, [])
   return (
     <Layout>
       <div>CLASSESROUTE</div>
