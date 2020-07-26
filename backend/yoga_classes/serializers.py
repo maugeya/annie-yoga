7d54b6eafb8491677af209f3985239a2c7685153
Angela Maugey <a.maugey@gmail.com>
2020-07-26 10:30:31 +0100

    Adding method to serializer to get user friendly time

--- backend/yoga_classes/serializers.py
+++ backend/yoga_classes/serializers.py
@@ -3,6 +3,13 @@ from .models import YogaClass
 
 
 class YogaClassSerializer(serializers.ModelSerializer):
+    time = serializers.SerializerMethodField()
+
     class Meta:
         model = YogaClass
         fields = '__all__'
+
+    def get_time(self, obj):
+        class_time = obj.time
+
+        return class_time.replace(hour=class_time.hour, minute=class_time.minute).strftime("%I:%M %p")
