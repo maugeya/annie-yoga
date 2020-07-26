9fbfe3af72d1c26b1a32ccbe26ee5bba2be1e838
Angela Maugey <a.maugey@gmail.com>
2020-07-26 10:30:16 +0100

    Adding django-cors-headers to project

--- backend/annie_may_rice/settings.py
+++ backend/annie_may_rice/settings.py
@@ -9,6 +9,8 @@ APP_ENV = os.environ.get('APP_ENV', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
@@ -21,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'yoga_classes',
 ]
@@ -28,6 +31,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
