4325ee3c39f2c76e340b1bab75d834a634f79610
Angela Maugey <a.maugey@gmail.com>
2020-07-26 20:47:23 +0100

    Adding logic to display classes and day title. Component is WIP

--- frontend/src/components/classesSection/ClassesSection.js
+++ frontend/src/components/classesSection/ClassesSection.js
@@ -1,18 +1,45 @@
 import React from "react"
+
+import styles from "./ClassesSection.module.css"
 import Layout from "../layout/Layout"
 import SectionHeader from "../common/sectionHeader/SectionHeader"
 import SectionText from "../common/sectionText/SectionText"
 
 export default function ClassesSection({ yogaClasses }) {
+  const renderYogaClasses = (yogaClasses, day) => {
+    const classesByDay = yogaClasses.filter(
+      (yogaClass) => yogaClass.day_of_week === day
+    )
+
+    return classesByDay.map((yogaClass, i) => (
+      <p key={i}>{`${yogaClass.where}, ${yogaClass.time}`}</p>
+    ))
+  }
+
+  const weekdaysWithClasses = [
+    ...new Set(yogaClasses.map((yogaClass) => yogaClass.day_of_week)),
+  ]
   return (
     <Layout>
       <section className="sectionContainer">
         <SectionHeader title="Public Classes" />
         <SectionText>
-          {yogaClasses.map((yogaClass, i) => (
-            <p key={i}>
-              {yogaClass.where},{yogaClass.day_of_week}, {yogaClass.time}
+          <div className={styles.aboutClasses}>
+            <h2 className={styles.sectionHeader}>About my classes</h2>
+            <p>
+              All public classes are vinyasa flow. I teach an accessible class
+              and welcome everyone to adapt the practice to suit their needs and
+              energy levels on any particular day. My intention is to allow
+              people to move their bodies in a way that feels nurturing and
+              beneficial and to gain strength, confidence and space in doing so.
             </p>
+          </div>
+
+          {weekdaysWithClasses.map((day, i) => (
+            <div key={i}>
+              <h2 className={styles.dayHeader}>{day}</h2>
+              {renderYogaClasses(yogaClasses, day)}
+            </div>
           ))}
         </SectionText>
       </section>
