ee8c2e7d3d6093cb6c4a5e98d5f789e5d7b951a9
Angela Maugey <a.maugey@gmail.com>
2020-07-28 19:31:03 +0100

    Basics of RetreatsSection WIP

---
+++ frontend/src/components/retreatsSection/RetreatsSection.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+import Layout from "../layout/Layout"
+import SectionHeader from "../common/sectionHeader/SectionHeader"
+import SectionText from "../common/sectionText/SectionText"
+
+export default function RetreatsSection() {
+  return (
+    <Layout>
+      <section className="sectionContainer">
+        <SectionHeader title="Upcoming retreats 2020" />
+        <SectionText>
+          <div>
+            <h3>Reset in Tuscany</h3>
+          </div>
+          <div>
+            <h3>Fire and Water</h3>
+          </div>
+        </SectionText>
+      </section>
+    </Layout>
+  )
+}
