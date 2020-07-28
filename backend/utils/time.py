76bf4b90bc0947066ce10a3dbdb4aa8542aa56ab
Angela Maugey <a.maugey@gmail.com>
2020-07-28 23:15:51 +0100

    Utils to check that months and years are the same in retreats

---
+++ backend/utils/time.py
@@ -0,0 +1,9 @@
+import datetime
+
+
+def start_month_and_end_month_are_equal(start_date, end_date):
+    return start_date.month == end_date.month
+
+
+def start_year_and_end_year_are_equal(start_date, end_date):
+    return start_date.year == end_date.year
