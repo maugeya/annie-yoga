268e6479183df3a6d4160e8fafa5238bb549f59e
Angela Maugey <a.maugey@gmail.com>
2020-07-28 13:49:47 +0100

    Implementing new image component in classes route

--- frontend/src/components/classes/ClassesRoute.js
+++ frontend/src/components/classes/ClassesRoute.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react"
 import styles from "./ClassesRoute.module.css"
 import LoadingSpinner from "../common/loadingSpinner/LoadingSpinner"
 import ClassesSection from "../classesSection/ClassesSection"
+import JumboImageContainer from "../common/jumboImageContainer/JumboImageContainer"
 
 export default function ClassesRoute() {
   const [yogaClasses, setYogaClasses] = useState([])
@@ -28,13 +29,12 @@ export default function ClassesRoute() {
 
   return (
     <div className={styles.container}>
-      <div className="jumboContainer">
+      <JumboImageContainer
+        headerTitle="Yoga"
+        headerSubTitle="Come yoga with me"
+      >
         <div className={`jumboImage ${styles.mainImage}`} />
-        <div className="rightTextOverlay">
-          <h1 className="animatedUnderline">Yoga</h1>
-          <h2>Come yoga with me</h2>
-        </div>
-      </div>
+      </JumboImageContainer>
       {isLoading && <LoadingSpinner />}
       <ClassesSection yogaClasses={yogaClasses} />
     </div>
