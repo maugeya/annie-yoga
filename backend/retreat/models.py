88f14b929adc57b628281e8b451f2ab2d0058867
Angela Maugey <a.maugey@gmail.com>
2020-07-28 19:50:50 +0100

    Allowing for country field to be blank on retreat location model

--- backend/retreat/models.py
+++ backend/retreat/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class RetreatLocation(models.Model):
     name = models.CharField(max_length=200)
     place = models.CharField(max_length=200)
-    country = models.CharField(max_length=100)
+    country = models.CharField(max_length=100, blank=True)
     url = models.URLField()
     is_active = models.BooleanField(default=True)
 
