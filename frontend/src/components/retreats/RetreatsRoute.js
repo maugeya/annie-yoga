e6b6cd593bf08df505ebe531f50f8ecd27021174
Angela Maugey <a.maugey@gmail.com>
2020-07-28 17:05:08 +0100

    Adding jumbo image to retreats page

--- frontend/src/components/retreats/RetreatsRoute.js
+++ frontend/src/components/retreats/RetreatsRoute.js
@@ -1,11 +1,18 @@
 import React from "react"
 
-import Layout from "../layout/Layout"
+import styles from "./RetreatsRoute.module.css"
+import JumboImageContainer from "../common/jumboImageContainer/JumboImageContainer"
 
 export default function RetreatsRoute() {
   return (
-    <Layout>
-      <div>RETREATSROUTE</div>
-    </Layout>
+    <div className={styles.container}>
+      <JumboImageContainer
+        headerTitle="Retreats and Workshops"
+        headerSubTitle="More yoga, retreats and workshops"
+        centerTextOverlay
+      >
+        <div className={`jumboImage ${styles.mainImage}`} />
+      </JumboImageContainer>
+    </div>
   )
 }
