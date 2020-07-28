e5b4441442ce64973eaf6676e3132f8a51cc8989
Angela Maugey <a.maugey@gmail.com>
2020-07-28 21:18:16 +0100

    View for retreats

--- backend/retreat/views.py
+++ backend/retreat/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Retreat
+from .serializers import RetreatSerializer
 
-# Create your views here.
+
+class ListRetreatsView(generics.ListAPIView):
+    queryset = Retreat.objects.all()
+    serializer_class = RetreatSerializer
