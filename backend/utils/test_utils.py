ef270022b5a18aab0cf15b6a6e12c14df72e5ce0
Angela Maugey <a.maugey@gmail.com>
2020-07-28 21:18:55 +0100

    Test utils to for testing dates in retreats

---
+++ backend/utils/test_utils.py
@@ -0,0 +1,16 @@
+import datetime
+import random
+
+
+def create_random_date():
+    start_date = datetime.date(2020, 1, 1)
+    end_date = datetime.date(2030, 2, 1)
+    time_between_dates = end_date - start_date
+    days_between_dates = time_between_dates.days
+    random_number_of_days = random.randrange(days_between_dates)
+
+    return start_date + datetime.timedelta(days=random_number_of_days)
+
+
+def add_week_to_date(start_date):
+    return start_date + datetime.timedelta(days=10)
