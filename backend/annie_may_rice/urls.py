194dd40f4a07cec3893c2390008f8576a5d25401
Angela Maugey <a.maugey@gmail.com>
2020-07-28 21:17:36 +0100

    Added URL for all retreats

--- backend/annie_may_rice/urls.py
+++ backend/annie_may_rice/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.conf.urls import include, url
 
 from yoga_classes import urls as yoga_classes_urls
+from retreat import urls as retreat_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^classes/', include(yoga_classes_urls)),
+    url(r'^retreats/', include(retreat_urls)),
 ]
