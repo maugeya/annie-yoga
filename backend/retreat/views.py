848560e08e622771d20456c35433b2fd48910663
Angela Maugey <a.maugey@gmail.com>
2020-07-29 20:20:37 +0100

    Filtering out inactive retreats and locations

--- backend/retreat/views.py
+++ backend/retreat/views.py
@@ -1,8 +1,11 @@
 from rest_framework import generics
+from django.db.models import Q
+
 from .models import Retreat
 from .serializers import RetreatSerializer
 
 
 class ListRetreatsView(generics.ListAPIView):
-    queryset = Retreat.objects.all()
+    queryset = Retreat.objects.filter(
+        Q(retreat_location__is_active=True) & Q(is_active=True))
     serializer_class = RetreatSerializer
