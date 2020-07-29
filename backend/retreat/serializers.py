763ba712484c2e73196373a9319cfb04ddfd270d
Angela Maugey <a.maugey@gmail.com>
2020-07-29 09:57:32 +0100

    Fixing functionality to work correctly

--- backend/retreat/serializers.py
+++ backend/retreat/serializers.py
@@ -19,7 +19,9 @@ class RetreatSerializer(serializers.ModelSerializer):
         fields = ('pretty_dates', 'retreat_location')
 
     def get_pretty_dates(self, obj):
-        if start_month_and_end_month_are_equal and start_year_and_end_year_are_equal:
+        retreat_start_date = obj.start_date
+        retreat_end_date = obj.end_date
+        if start_month_and_end_month_are_equal(start_date=retreat_start_date, end_date=retreat_end_date) and start_year_and_end_year_are_equal(start_date=retreat_start_date, end_date=retreat_end_date):
             return "{start_day} - {end_day} {month} {year}"\
                 .format(
                     start_day=obj.start_date.strftime("%d"),
@@ -27,16 +29,7 @@ class RetreatSerializer(serializers.ModelSerializer):
                     month=obj.start_date.strftime("%B"),
                     year=obj.start_date.strftime("%Y")
                 )
-        elif not start_month_and_end_month_are_equal:
-            return "{start_day} {start_month} - {end_day} {end_month} {year}"\
-                .format(
-                    start_day=obj.start_date.strftime("%d"),
-                    start_month=obj.start_date.strftime("%B"),
-                    end_day=obj.end_date.strftime("%d"),
-                    end_month=obj.end_date.strftime("%B"),
-                    year=obj.start_date.strftime("%Y")
-                )
-        elif not start_year_and_end_year_are_equal:
+        elif not start_month_and_end_month_are_equal(start_date=retreat_start_date, end_date=retreat_end_date) and not start_year_and_end_year_are_equal(start_date=retreat_start_date, end_date=retreat_end_date):
             return "{start_day} {start_month} {start_year} - {end_day} {end_month} {end_year}"\
                 .format(
                     start_day=obj.start_date.strftime("%d"),
@@ -45,4 +38,13 @@ class RetreatSerializer(serializers.ModelSerializer):
                     end_day=obj.end_date.strftime("%d"),
                     end_month=obj.end_date.strftime("%B"),
                     end_year=obj.end_date.strftime("%Y")
+                )
+        elif not start_month_and_end_month_are_equal(start_date=retreat_start_date, end_date=retreat_end_date):
+            return "{start_day} {start_month} - {end_day} {end_month} {year}"\
+                .format(
+                    start_day=obj.start_date.strftime("%d"),
+                    start_month=obj.start_date.strftime("%B"),
+                    end_day=obj.end_date.strftime("%d"),
+                    end_month=obj.end_date.strftime("%B"),
+                    year=obj.start_date.strftime("%Y")
                 )
