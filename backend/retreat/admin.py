cb54b75c124ac9bc10d05fbeefaa589d70622a34
Angela Maugey <a.maugey@gmail.com>
2020-07-29 20:20:20 +0100

    Retreat models added to django admin. WIP to make inline

--- backend/retreat/admin.py
+++ backend/retreat/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Retreat, RetreatLocation
 
-# Register your models here.
+
+class RetreatLocationAdmin(admin.ModelAdmin):
+    model = RetreatLocation
+    list_display = ('name', 'is_active')
+
+
+class RetreatAdmin(admin.ModelAdmin):
+    model = Retreat
+    list_display = ('start_date', 'end_date', 'is_active')
+    ordering = ('start_date',)
+
+
+admin.site.register(Retreat, RetreatAdmin)
+admin.site.register(RetreatLocation, RetreatLocationAdmin)
