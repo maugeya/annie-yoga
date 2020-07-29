fee0d17d4dd5d3f7e9076f89ccbd61ccb8897f94
Angela Maugey <a.maugey@gmail.com>
2020-07-29 13:59:25 +0100

    Implementing new useFetch hook and temporary handle of error

--- frontend/src/components/classes/ClassesRoute.js
+++ frontend/src/components/classes/ClassesRoute.js
@@ -4,29 +4,14 @@ import styles from "./ClassesRoute.module.css"
 import LoadingSpinner from "../common/loadingSpinner/LoadingSpinner"
 import ClassesSection from "../classesSection/ClassesSection"
 import JumboImageContainer from "../common/jumboImageContainer/JumboImageContainer"
+import useFetch from "../../hooks/useFetch"
 
 export default function ClassesRoute() {
-  const [yogaClasses, setYogaClasses] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
-  const [error, setError] = useState([])
+  const res = useFetch("http://localhost:8000/classes/", {})
+  if (res.error.length) console.log("ERROR WIP", res.error)
 
-  useEffect(() => {
-    async function fetchData() {
-      const res = await fetch("http://localhost:8000/classes/")
-      res
-        .json()
-        .then((res) => {
-          setYogaClasses(res)
-          setIsLoading(false)
-        })
-        .catch((err) => {
-          setIsLoading(false)
-          setError(err)
-        })
-    }
-    fetchData()
-  }, [])
-
+  const yogaClasses = res.data
+  const isLoading = res.isLoading
   return (
     <div className={styles.container}>
       <JumboImageContainer
