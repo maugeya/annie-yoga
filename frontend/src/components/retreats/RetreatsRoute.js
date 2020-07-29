e7fd097db794b16209455365cf2bcb391d5ff43f
Angela Maugey <a.maugey@gmail.com>
2020-07-29 14:08:10 +0100

    Using useFetch hook and adding loading spinner to route

--- frontend/src/components/retreats/RetreatsRoute.js
+++ frontend/src/components/retreats/RetreatsRoute.js
@@ -3,8 +3,15 @@ import React from "react"
 import styles from "./RetreatsRoute.module.css"
 import JumboImageContainer from "../common/jumboImageContainer/JumboImageContainer"
 import RetreatsSection from "../retreatsSection/RetreatsSection"
+import LoadingSpinner from "../common/loadingSpinner/LoadingSpinner"
+import useFetch from "../../hooks/useFetch"
 
 export default function RetreatsRoute() {
+  const res = useFetch("http://localhost:8000/retreats/", {})
+  if (res.error.length) console.log("ERROR WIP", res.error)
+
+  const retreats = res.data
+  const isLoading = res.isLoading
   return (
     <div className={styles.container}>
       <JumboImageContainer
@@ -14,7 +21,8 @@ export default function RetreatsRoute() {
       >
         <div className={`jumboImage ${styles.mainImage}`} />
       </JumboImageContainer>
-      <RetreatsSection />
+      {isLoading && <LoadingSpinner />}
+      <RetreatsSection retreats={retreats} />
     </div>
   )
 }
