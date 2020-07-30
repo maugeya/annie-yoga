5e885994c53d33e79f75bbb56f5af4bc1b1757b5
Angela Maugey <a.maugey@gmail.com>
2020-07-30 10:15:39 +0100

    Correctly rendering retreat dates

--- frontend/src/components/retreatsSection/RetreatsSection.js
+++ frontend/src/components/retreatsSection/RetreatsSection.js
@@ -5,17 +5,42 @@ import SectionHeader from "../common/sectionHeader/SectionHeader"
 import SectionText from "../common/sectionText/SectionText"
 
 export default function RetreatsSection({ retreats }) {
+  const uniqueRetreatNames = [
+    ...new Set(retreats.map((retreat) => retreat.retreat_location.name)),
+  ]
+
+  const renderRetreatDetails = (retreats, name) => {
+    const retreatsByDay = retreats.filter(
+      (retreat) => retreat.retreat_location.name === name
+    )
+    return retreatsByDay.map((retreat, i) => {
+      return (
+        <div key={i}>
+          <p>{retreat.pretty_dates}</p>
+          <a
+            className="inlineLink"
+            href={retreat.retreat_location.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p>{`${retreat.retreat_location.place}, ${retreat.retreat_location.country}`}</p>
+          </a>
+        </div>
+      )
+    })
+  }
+
   return (
     <Layout>
       <section className="sectionContainer">
         <SectionHeader title="Upcoming retreats 2020" />
         <SectionText>
-          <div>
-            <h3>Reset in Tuscany</h3>
-          </div>
-          <div>
-            <h3>Fire and Water</h3>
-          </div>
+          {uniqueRetreatNames.map((name, i) => (
+            <div key={i}>
+              <h3>{name}</h3>
+              {renderRetreatDetails(retreats, name)}
+            </div>
+          ))}
         </SectionText>
       </section>
     </Layout>
