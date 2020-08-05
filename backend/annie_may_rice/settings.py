c5428a6ecb13b07bfc46a3d6c45014bb9810a149
Angela Maugey <a.maugey@gmail.com>
2020-08-05 09:42:22 +0100

    Changing settings for AWS credentials to not show

--- backend/annie_may_rice/settings.py
+++ backend/annie_may_rice/settings.py
@@ -105,6 +105,8 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', '')
 AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', '')
+AWS_DEFAULT_ACL = None
+AWS_QUERYSTRING_AUTH = False
 
 
 # Internationalization
