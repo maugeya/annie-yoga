478f9a68794e235534d4d61f493250041c9bb4c2
Angela Maugey <a.maugey@gmail.com>
2020-08-08 14:50:33 +0100

    Updating how locations and details are displayed

--- frontend/src/components/retreatsSection/RetreatsSection.js
+++ frontend/src/components/retreatsSection/RetreatsSection.js
@@ -9,6 +9,13 @@ export default function RetreatsSection({ retreats }) {
     ...new Set(retreats.map((retreat) => retreat.retreat_location.name)),
   ]
 
+  const retreat_locations = []
+  retreats.map((retreat) => retreat_locations.push(retreat.retreat_location))
+
+  const uniqueRetreatLocations = [
+    ...new Map(retreat_locations.map((item) => [item.id, item])).values(),
+  ]
+
   const renderRetreatDetails = (retreats, name) => {
     const retreatsByDay = retreats.filter(
       (retreat) => retreat.retreat_location.name === name
@@ -17,28 +24,24 @@ export default function RetreatsSection({ retreats }) {
       return (
         <div key={i}>
           <p>{retreat.pretty_dates}</p>
-          <a
-            className="inlineLink"
-            href={retreat.retreat_location.url}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <p>{`${retreat.retreat_location.place}, ${retreat.retreat_location.country}`}</p>
-          </a>
         </div>
       )
     })
   }
-
   return (
     <Layout>
       <section className="sectionContainer">
         <SectionHeader title="Upcoming retreats 2020" />
         <SectionText>
-          {uniqueRetreatNames.map((name, i) => (
+          {uniqueRetreatLocations.map((retreat_location, i) => (
             <div key={i}>
-              <h3>{name}</h3>
-              {renderRetreatDetails(retreats, name)}
+              <h3>{retreat_location.name}</h3>
+              {renderRetreatDetails(retreats, retreat_location.name)}
+              <a className="inlineLink" href={retreat_location.url}>
+                <p>
+                  {retreat_location.place}, {retreat_location.country}
+                </p>
+              </a>
             </div>
           ))}
         </SectionText>
