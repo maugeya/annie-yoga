a2962dc016f19d24c9f7548f7994eb5acd2eb67a
Angela Maugey <a.maugey@gmail.com>
2020-08-10 14:19:45 +0100

    Simplifying retreat section by adding in card component

--- frontend/src/components/retreatsSection/RetreatsSection.js
+++ frontend/src/components/retreatsSection/RetreatsSection.js
@@ -1,49 +1,34 @@
 import React from "react"
 
+import styles from "./RetreatsSection.module.css"
 import Layout from "../layout/Layout"
 import SectionHeader from "../common/sectionHeader/SectionHeader"
 import SectionText from "../common/sectionText/SectionText"
+import RetreatCard from "./RetreatCard.js"
+import {
+  getDatesForRetreatLocation,
+  getUniqueRetreatLocations,
+} from "../../utils/retreats"
 
 export default function RetreatsSection({ retreats }) {
-  const uniqueRetreatNames = [
-    ...new Set(retreats.map((retreat) => retreat.retreat_location.name)),
-  ]
-
-  const retreat_locations = []
-  retreats.map((retreat) => retreat_locations.push(retreat.retreat_location))
-
-  const uniqueRetreatLocations = [
-    ...new Map(retreat_locations.map((item) => [item.id, item])).values(),
-  ]
-
-  const renderRetreatDetails = (retreats, name) => {
-    const retreatsByDay = retreats.filter(
-      (retreat) => retreat.retreat_location.name === name
-    )
-    return retreatsByDay.map((retreat, i) => {
-      return (
-        <div key={i}>
-          <p>{retreat.pretty_dates}</p>
-        </div>
-      )
-    })
-  }
+  const uniqueRetreatLocations = getUniqueRetreatLocations(retreats)
   return (
     <Layout>
       <section className="sectionContainer">
         <SectionHeader title="Upcoming retreats 2020" />
         <SectionText>
-          {uniqueRetreatLocations.map((retreat_location, i) => (
-            <div key={i}>
-              <h3>{retreat_location.name}</h3>
-              {renderRetreatDetails(retreats, retreat_location.name)}
-              <a className="inlineLink" href={retreat_location.url}>
-                <p>
-                  {retreat_location.place}, {retreat_location.country}
-                </p>
-              </a>
-            </div>
-          ))}
+          <div className={styles.cardTableContainer}>
+            {uniqueRetreatLocations.map((retreat_location, i) => (
+              <RetreatCard
+                retreat_dates={getDatesForRetreatLocation(
+                  retreats,
+                  retreat_location.id
+                )}
+                retreat_location={retreat_location}
+                key={i}
+              />
+            ))}
+          </div>
         </SectionText>
       </section>
     </Layout>
