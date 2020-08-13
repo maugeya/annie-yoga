a8e06a09182c09dd69c8fa7f1b85e8898f4ef13a
Angela Maugey <a.maugey@gmail.com>
2020-08-13 14:40:44 +0100

    basic update for RetreatLocationDetail component

--- frontend/src/components/retreatDetails/RetreatLocationDetailRoute.js
+++ frontend/src/components/retreatDetails/RetreatLocationDetailRoute.js
@@ -1,11 +1,8 @@
 import React from "react"
 
-export default function RetreatLocationDetailRoute({ retreatDetails }) {
-  return (
-    <div>
-      {retreatDetails.map((retreat) => (
-        <p>{retreat.pretty_dates}</p>
-      ))}
-    </div>
-  )
+export default function RetreatLocationDetailRoute({
+  retreatLocation,
+  retreatDates,
+}) {
+  return <div>{retreatDates}</div>
 }
