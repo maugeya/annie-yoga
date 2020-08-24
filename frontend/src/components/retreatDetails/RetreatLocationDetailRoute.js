c4f277f9565c42a129516b5c47b7522d423b57a9
Angela Maugey <a.maugey@gmail.com>
2020-08-24 17:08:59 +0100

    WIP initial code for RetreatLocationDetailRoute

--- frontend/src/components/retreatDetails/RetreatLocationDetailRoute.js
+++ frontend/src/components/retreatDetails/RetreatLocationDetailRoute.js
@@ -1,8 +1,23 @@
 import React from "react"
+import JumboImageContainer from "../common/jumboImageContainer/JumboImageContainer"
+import RetreatLocationDetailSection from "./RetreatLocationDetailSection"
 
 export default function RetreatLocationDetailRoute({
   retreatLocation,
   retreatDates,
 }) {
-  return <div>{retreatDates}</div>
+  return (
+    <div className="container">
+      <JumboImageContainer
+        headerTitle={retreatLocation.name}
+        headerSubTitle={retreatLocation.subtitle}
+        centerTextOverlay
+        imageUrl={`${retreatLocation.retreatMainImageS3Url}`}
+      />
+      <RetreatLocationDetailSection
+        retreatLocation={retreatLocation}
+        retreatDates={retreatDates}
+      />
+    </div>
+  )
 }
