a97bb2a27b12016a89ee4db57ad2e01e54c27153
Angela Maugey <a.maugey@gmail.com>
2020-08-25 17:48:37 +0100

    Creating an image grid and styles for common use

---
+++ frontend/src/components/common/imageGrid/AppImageGrid.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import styles from "./AppImageGrid.module.css"
+
+export default function AppImageGrid({ imageURLs }) {
+  return (
+    <div className={styles.galleryContainer}>
+      {imageURLs.map((imageURL, i) => (
+        <div className={styles.imageContainer} key={i}>
+          <img src={imageURL} alt="retreat-picture" />
+        </div>
+      ))}
+    </div>
+  )
+}
