4a73f659a592d6cc81717df424ce0ad154a46944
Angela Maugey <a.maugey@gmail.com>
2020-09-03 14:51:03 +0100

    WIP completing RecipeDetailRoute

--- frontend/src/components/recipeDetail/RecipeDetailRoute.js
+++ frontend/src/components/recipeDetail/RecipeDetailRoute.js
@@ -1,5 +1,16 @@
 import React from "react"
+import RecipeDetailSection from "../recipeDetailSection/RecipeDetailSection"
+import JumboImageContainer from "../common/jumboImageContainer/JumboImageContainer"
 
 export default function RecipeDetailRoute({ recipe }) {
-  return <div>RECIPE DETAILS TO GO HERE</div>
+  return (
+    <div>
+      <JumboImageContainer
+        headerTitle={recipe.title}
+        centerTextOverlay
+        imageUrl={recipe.imageS3}
+      />
+      <RecipeDetailSection recipe={recipe} />
+    </div>
+  )
 }
