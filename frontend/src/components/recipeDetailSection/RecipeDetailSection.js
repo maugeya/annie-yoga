d261ac433343c4496b273f5c88c5c83b1c3e03da
Angela Maugey <a.maugey@gmail.com>
2020-09-04 17:51:49 +0100

    Update to RecipeDetailSection. beginnings of fleshing out individual recipe pages

--- frontend/src/components/recipeDetailSection/RecipeDetailSection.js
+++ frontend/src/components/recipeDetailSection/RecipeDetailSection.js
@@ -6,20 +6,48 @@ import SectionHeader from "../common/sectionHeader/SectionHeader"
 import SectionText from "../common/sectionText/SectionText"
 
 export default function RecipeDetailSection({ recipe }) {
-  const renderRecipeIntro = (recipe) => {
-    const { recipeIntro } = recipe
-    return recipeIntro.map((line) => <p>{line}</p>)
+  const { recipeIntro, method, recipeIngredients } = recipe
+
+  const renderRecipeIntro = () => {
+    return recipeIntro.map((line, i) => <p key={i}>{line}</p>)
+  }
+
+  const renderIngredientsList = () => {
+    return recipeIngredients.map((ingredient, i) => (
+      <li key={i}>
+        <p>
+          {ingredient.prettyIngredient}{" "}
+          {ingredient.isOptional && (
+            <span className={styles.ingredientOptional}>(optional)</span>
+          )}
+        </p>
+      </li>
+    ))
+  }
+
+  const renderMethodList = () => {
+    return method.map((step, i) => (
+      <li key={i}>
+        <p>{step}</p>
+      </li>
+    ))
   }
 
   return (
     <Layout>
       <section className={styles.recipeIntroSection}>
-        <div className={styles.recipeIntro}>{renderRecipeIntro(recipe)}</div>
+        <div className={styles.recipeIntro}>{renderRecipeIntro()}</div>
       </section>
-      <section className="sectionContainer">
+      <section className={`sectionContainer ${styles.recipeIngredientSection}`}>
         <SectionHeader title="You will need" />
         <SectionText>
-          <p>INGREDIENTS HERE</p>
+          <ul>{renderIngredientsList()}</ul>
+        </SectionText>
+      </section>
+      <section className="sectionContainer">
+        <SectionHeader title="Make it" />
+        <SectionText>
+          <ol>{renderMethodList()}</ol>
         </SectionText>
       </section>
     </Layout>
