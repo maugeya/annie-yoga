0863f456b5b0c9ab3648290e4ae164098bcda3ad
Angela Maugey <a.maugey@gmail.com>
2020-09-07 19:39:15 +0100

    Updating for burgermenu to close on link click

--- frontend/src/components/burgerMenu/BurgerMenu.js
+++ frontend/src/components/burgerMenu/BurgerMenu.js
@@ -3,33 +3,62 @@ import { Link } from "react-router-dom"
 
 import styles from "./BurgerMenu.module.css"
 
-const BurgerMenu = ({ open }) => {
+const BurgerMenu = ({ open, setOpen }) => {
   const menuPostitionStyles = open ? styles.open : ""
+
   return (
     <nav className={`${styles.container} ${menuPostitionStyles}`} open={open}>
-      <img
-        className={styles.logo}
-        src={require("../../assets/annie-logo.png")}
-        alt="Annie May Rice logo"
-      />
-      <Link className={styles.menuLinks} to="/">
-        About
-      </Link>
-      <Link className={styles.menuLinks} to="/classes">
-        Classes
-      </Link>
-      <Link className={styles.menuLinks} to="/retreats">
-        Retreats
-      </Link>
-      <Link className={styles.menuLinks} to="/corporate">
-        Corporate
-      </Link>
-      <Link className={styles.menuLinks} to="/food">
-        Food
-      </Link>
-      <Link className={styles.menuLinks} to="/blog">
-        Blog
-      </Link>
+      <div className={styles.logoContainer}>
+        <img
+          className={styles.logo}
+          src={require("../../assets/annie-logo.png")}
+          alt="Annie May Rice logo"
+        />
+      </div>
+      <div className={styles.linkContainer}>
+        <Link
+          className={styles.menuLinks}
+          to="/"
+          onClick={() => setOpen(false)}
+        >
+          About
+        </Link>
+        <Link
+          className={styles.menuLinks}
+          to="/classes"
+          onClick={() => setOpen(false)}
+        >
+          Classes
+        </Link>
+        <Link
+          className={styles.menuLinks}
+          to="/retreats"
+          onClick={() => setOpen(false)}
+        >
+          Retreats
+        </Link>
+        <Link
+          className={styles.menuLinks}
+          to="/corporate"
+          onClick={() => setOpen(false)}
+        >
+          Corporate
+        </Link>
+        <Link
+          className={styles.menuLinks}
+          to="/food"
+          onClick={() => setOpen(false)}
+        >
+          Food
+        </Link>
+        <Link
+          className={styles.menuLinks}
+          to="/blog"
+          onClick={() => setOpen(false)}
+        >
+          Blog
+        </Link>
+      </div>
     </nav>
   )
 }
